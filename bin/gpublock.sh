fce6228bea971cf243f3c79ab95b0ca20578f909
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-06-25 18:10:45 -0500

    Commit from shell alias at 06/25/23 06:10:45 PM

--- bin/gpublock.sh
+++ bin/gpublock.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/dash
 
+[ "$(pgrep "${0##*/}" | wc -l)" -gt 2 ] && exit
+
+echo 'blocking GPU sleep'
+# [ "$(xrandr | grep -c ' connected')" -gt 1 ] || 
+
 if [ "$(hyprctl monitors | grep -c '^Monitor')" -gt 1 ]; then
     while true; do
-        nvidia-smi
-        sleep 5
+        nvidia-smi >/dev/null
     done
 fi
 
