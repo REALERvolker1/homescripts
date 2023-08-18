d3b89d5e719908dc5cd61d42cb558dd7ba299ad3
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-08-17 21:47:27 -0500

    Commit from shell alias at 08/17/23 09:47:27 PM

--- .config/hypr/scripts/pluginload.sh
+++ .config/hypr/scripts/pluginload.sh
@@ -5,6 +5,8 @@ hyprlib='/usr/lib64/hyprland'
 plugins=(
     "$HOME/.local/src/hyprfocus/hyprfocus.so"
     "$hyprlib/libborders-plus-plus.so"
+    "$hyprlib/libcsgo-vulkan-fix.so"
+    #"$HOME/.local/src/hy3/build/libhy3.so"
 )
 
 for i in "${plugins[@]}"; do
