a43526c0a1e3317bbc9d4e7b5c2480ba4d591dcb
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-08-21 19:40:06 -0500

    Commit from shell alias at 08/21/23 07:40:06 PM

--- .config/zsh/settings/fzf-preview.sh
+++ .config/zsh/settings/fzf-preview.sh
@@ -3,7 +3,7 @@
 
 set -eu
 
-err_function () {
+err_function() {
     [ -n "${1:-}" ] && echo "${1:-}"
     figlet -- fzf-tab
     exit 1
@@ -21,31 +21,28 @@ mime="$(file -bL --mime-type "$path")"
 width="$(echo "$(tput cols) / 2 - 8" | bc)"
 
 case "$mime" in
-    *'/directory')
-        lsd -ld "$path"
-        lsd "$path"
+*'/directory')
+    lsd -ld "$path"
+    lsd "$path"
     ;;
-    'image/'*)
-        #timg -g40x60 "$path"
-        #timg "-g${width}x1000" "$path"
-        chafa "--size=${width}x6000" --format='symbols' "$path"
-        exiftool "$path"
+'image/'*)
+    chafa "--size=${width}x6000" --format='symbols' "$path"
+    exiftool "$path"
     ;;
-    *'/vnd.openxmlformats-officedocument.spreadsheetml.sheet' | *'/vnd.ms-excel')
-        in2csv "$path" | xsv table | bat -ltsv --color=always
+*'/vnd.openxmlformats-officedocument.spreadsheetml.sheet' | *'/vnd.ms-excel')
+    in2csv "$path" | xsv table | bat -ltsv --color=always
     ;;
-    'text/'*)
-        bat --color=always --line-range :200 "$path"
+'text/'*)
+    bat --color=always --line-range :200 "$path"
     ;;
-    *'/vnd.oasis.opendocument.text')
-        odt2txt "$path"
+*'/vnd.oasis.opendocument.text')
+    odt2txt "$path"
     ;;
-    *'/pdf')
-        pdf2svg "$path" "$XDG_RUNTIME_DIR/pdf2svg-preview.svg"
-        chafa "--size=${width}x6000" --format='symbols' "$XDG_RUNTIME_DIR/pdf2svg-preview.svg"
+*'/pdf')
+    pdf2svg "$path" "$XDG_RUNTIME_DIR/pdf2svg-preview.svg"
+    chafa "--size=${width}x6000" --format='symbols' "$XDG_RUNTIME_DIR/pdf2svg-preview.svg"
     ;;
-    *)
-        err_function "$path"
+*)
+    err_function "$path"
     ;;
 esac
-
