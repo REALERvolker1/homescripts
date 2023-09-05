910e9527db0faa4a0a59d24ea4a96a1fcf19283d
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-09-05 17:19:33 -0500

    Commit from shell alias at 09/05/23 05:19:33 PM

--- bin/rofi-power-menu.sh
+++ bin/rofi-power-menu.sh
@@ -3,25 +3,41 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if ! command -v systemctl &>/dev/null; then
+    echo "Error, this requires systemd!"
+    exit 2
+fi
+
 declare -A actions
 actions[logout]="Log Out"
 actions[reboot]="Restart"
 actions[sleep]="Sleep"
 actions[shutdown]="Shut Down"
+actions[advanced]="Advanced options"
 
 declare -A advanced_actions
 advanced_actions[tty]="Switch active TTY"
 advanced_actions[uefi]="UEFI reboot"
 advanced_actions[hibernate]="Hibernate"
+if [ -S "${YDOTOOL_SOCKET:-/tmp/.ydotool_socket}" ]; then
+    advanced_actions_str="${advanced_actions[tty]}
+${advanced_actions[uefi]}
+${advanced_actions[hibernate]}"
+else
+    advanced_actions_str="${advanced_actions[uefi]}
+${advanced_actions[hibernate]}"
+fi
 
-if command -v flatpak &>/dev/null; then
-    declare -a unruly_flatpaks=(
-        'com.valvesoftware.Steam'
-        'com.discordapp.Discord'
-    )
-    _kill_flatpaks () {
-        local grepstr="$(printf '%s|' "${unruly_flatpaks[@]}")"
-        local unruly_procs="$(flatpak ps 2>/dev/null | grep -oE "(${grepstr:: -1})" | uniq)"
+declare -a unruly_flatpaks=(
+    'com.valvesoftware.Steam'
+    'com.discordapp.Discord'
+)
+_kill_flatpaks() {
+    if command -v flatpak &>/dev/null; then
+        local grepstr
+        grepstr="$(printf '%s|' "${unruly_flatpaks[@]}")"
+        local unruly_procs
+        unruly_procs="$(flatpak ps 2>/dev/null | grep -oE "(${grepstr::-1})" | uniq)"
         if [ -n "$unruly_procs" ]; then
             echo "Unruly flatpaks detected. Killing them"
             for i in $unruly_procs; do
@@ -29,12 +45,79 @@ if command -v flatpak &>/dev/null; then
             done
             wait
         fi
+    else
+        echo "Flatpak is not installed. Skipping unruly flatpaks"
+    fi
+}
+
+_tty_switch() {
+    local selected_tty event_codes
+    local ctrl alt f_code
+    selected_tty="$(printf 'Switch to TTY %s\n' {1..10} | rofi -dmenu)"
+    [ -z "${selected_tty:-}" ] && exit 1
+    local tty_number="${selected_tty##* }"
+    event_codes="$(cat '/usr/include/linux/input-event-codes.h')"
+    _keycode() {
+        echo "$event_codes" | grep -m 1 -oP "#define ${1}\t*\K.*"
     }
-fi
 
-#_check_flatpaks
+    ctrl="$(_keycode 'KEY_LEFTCTRL')"
+    alt="$(_keycode 'KEY_LEFTALT')"
+    f_code="$(_keycode "KEY_F${tty_number}")"
 
-menu_1="$(printf '%s\n' "${actions[@]}" | rofi -dmenu)"
+    _kill_flatpaks
+    command -v playerctl &>/dev/null && playerctl pause
+    echo "[${0##*/}] Switched TTY to TTY ${tty_number}"
+
+    ydotool key "${ctrl}:1" "${alt}:1" "${f_code}:1" "${f_code}:0" "${alt}:0" "${ctrl}:0"
+}
+
+menu_1="$(
+    printf '%s\n' \
+        "${actions[shutdown]}" \
+        "${actions[reboot]}" \
+        "${actions[sleep]}" \
+        "${actions[advanced]}" \
+        "${actions[logout]}" | rofi -dmenu
+)"
 echo "$menu_1"
 
-
+case "${menu_1:-}" in
+"${actions[shutdown]}")
+    _kill_flatpaks
+    systemctl poweroff
+    ;;
+"${actions[reboot]}")
+    _kill_flatpaks
+    systemctl reboot
+    ;;
+"${actions[sleep]}")
+    systemctl sleep
+    ;;
+"${actions[advanced]}")
+    menu_2="$(printf '%s\n' "$advanced_actions_str" | rofi -dmenu)"
+    case "${menu_2:-}" in
+    "${advanced_actions[tty]}")
+        _tty_switch
+        ;;
+    "${advanced_actions[uefi]}")
+        systemctl reboot --firmware-setup
+        ;;
+    "${advanced_actions[hibernate]}")
+        systemctl hibernate
+        ;;
+    *)
+        echo "Error, advanced action not specified"
+        exit 1
+        ;;
+    esac
+    ;;
+"${actions[logout]}")
+    _kill_flatpaks
+    loginctl kill-session "$XDG_SESSION_ID"
+    ;;
+*)
+    echo "Error, action not specified"
+    exit 1
+    ;;
+esac
--- bin/test-bash.sh
+++ bin/test-bash.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/bash
 
-CHEAT_FILE="$XDG_RUNTIME_DIR/dumbfetch_cheat_file.txt"
+__vlkenv_data_dirs=''
 
-props=(
-    "$((${SHLVL:-1} - 1))"
-    "$(uptime -p | sed 's/^up // ; s/hour/hr/g ; s/minute/min/g')"
-    "${TERM:-Undefined}"
-    "$(echo "$info_file" | cut -f 1)"
-    "$(echo "$info_file" | cut -f 2)"
-    "$(echo "$info_file" | cut -f 3)"
-)
+oldifs="$IFS"
+IFS=':'
+for i in $XDG_DATA_DIRS; do
+    [ ! -d "$i" ] && continue
+    __vlkenv_data_dirs="$i:${__vlkenv_data_dirs}"
+done
+IFS="$oldifs"
 
-len="$(printf '%s\n' "${props[@]}" | wc -L)"
-echo "$len"
+echo "$__vlkenv_data_dirs"
