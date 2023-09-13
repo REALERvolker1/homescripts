abc65afe086efd58dd6c57f369ecbf97a0490a17
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-09-12 22:36:27 -0500

    Commit from shell alias at 09/12/23 10:36:27 PM

--- .config/shell/vlkprompt-rs/src/config-bak.rs
+++ .config/shell/vlkprompt-rs/src/config-bak.rs
@@ -1,23 +1,22 @@
 use std::{
     io,
     process,
-    error,
     env,
-    fs
 };
 use serde::{Serialize, Deserialize};
+
+// #[derive(Debug, PartialEq, Clone, Copy)]
+// pub enum IconType {
+//     PowerLine,
+//     DashLine,
+//     FallBack
+// }
 
 // I might need to expand this to include ksh or ash
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Copy)]
 pub enum Shell {
     Bash,
     Zsh,
-}
-#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Copy)]
-pub enum DirType {
-    Cwd,
-    Git,
-    Vim,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -58,18 +57,6 @@ pub struct Config {
     pub icons: Icons,
     pub colors: ColorScheme,
     pub end_icon: String,
-}
-#[derive(Debug, PartialEq, Clone)]
-pub struct Options {
-    pub has_err: bool,
-    pub error: u32,
-    pub has_jobs: bool,
-    pub jobs: u32,
-    pub has_sudo: bool,
-    pub cwd_type: DirType,
-    pub is_cwd_writable: bool,
-    pub cwd: String,
-    pub is_transient: bool,
 }
 
 macro_rules! color {
@@ -163,6 +150,7 @@ macro_rules! high_color {
     };
 }
 
+
 pub fn generate_config(current_shell: &str) -> Result<Config, io::Error> {
     let shell = match current_shell.to_ascii_lowercase().as_str() {
         "zsh" => Shell::Zsh,
@@ -221,167 +209,4 @@ pub fn generate_config(current_shell: &str) -> Result<Config, io::Error> {
         colors: colors,
         end_icon: end_icon.to_string(),
     })
-}
-
-fn get_env_optional_int(key: &str) -> Result<(bool, u32), env::VarError> {
-    let env_var = env::var(key)?.parse::<u32>().unwrap_or(69420); // magic number that is big enough for you to notice
-    let env_bool;
-    if env_var != 0 {
-        env_bool = true;
-    }
-    else {
-        env_bool = false;
-    }
-    Ok((env_bool, env_var))
-}
-fn get_env_optional_bool(key: &str) -> Result<bool, env::VarError> {
-    let env_var = env::var(key)?;
-    Ok(match env_var.as_str() {
-        "true" => true,
-        _ => false
-    })
-}
-
-pub fn get_opts() -> Result<Options, Box<dyn error::Error>> {
-    let env_err = get_env_optional_int("VLKPROMPT_ERR")?;
-    let env_jobs = get_env_optional_int("VLKPROMPT_JOBS")?;
-    let env_sudo = get_env_optional_bool("VLKPROMPT_SUDO")?;
-    let env_transient = get_env_optional_bool("VLKPROMPT_TRANSIENT")?;
-
-    let env_type;
-    if get_env_optional_bool("VLKPROMPT_VIM")? {
-        env_type = DirType::Vim
-    }
-    else if get_env_optional_bool("VLKPROMPT_GIT")? {
-        env_type = DirType::Git
-    }
-    else {
-        env_type = DirType::Cwd
-    }
-
-    let current_dir = env::current_dir()?;
-    let writable = fs::metadata(&current_dir)?.permissions().readonly();
-    let cwd_string = current_dir.to_string_lossy().replace(&env::var("HOME").unwrap(),"~");
-
-    let opts = Options {
-        has_err: env_err.0,
-        error: env_err.1,
-        has_jobs: env_jobs.0,
-        jobs: env_jobs.1,
-        has_sudo: env_sudo,
-        cwd_type: env_type,
-        is_cwd_writable: !writable,
-        cwd: cwd_string,
-        is_transient: env_transient,
-    };
-    Ok(opts)
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct DefinedConfig {
-    pub err: String,
-    pub err_job: String,
-    pub err_dir: String,
-    pub job: String,
-    pub job_dir: String,
-    pub dir: String,
-    pub dir_end: String,
-    pub dir_sud: String,
-}
-pub fn define_config(cfg: Config, opt: Options) -> Result<DefinedConfig, Box<dyn error::Error>> {
-
-    let dir_bg;
-    let dir_fg;
-    let dir_text;
-    let dir_icon;
-    match opt.cwd_type {
-        DirType::Cwd => {
-            dir_bg = &cfg.colors.background.dir;
-            dir_fg = &cfg.colors.foreground.dir;
-            dir_text = &cfg.colors.text_light;
-            if &opt.is_cwd_writable == &true {
-                dir_icon = &cfg.icons.fs_rw
-            } else {
-                dir_icon = &cfg.icons.fs_ro
-            }
-        },
-        DirType::Git => {
-            dir_bg = &cfg.colors.background.git;
-            dir_fg = &cfg.colors.foreground.git;
-            dir_text = &cfg.colors.text_dark;
-            dir_icon = &cfg.icons.git;
-        },
-        DirType::Vim => {
-            dir_bg = &cfg.colors.background.vim;
-            dir_fg = &cfg.colors.foreground.vim;
-            dir_text = &cfg.colors.text_dark;
-            dir_icon = &cfg.icons.vim;
-        }
-    }
-    let def_conf = DefinedConfig {
-        err: format!("{}{}{} {} {} {}",
-            &cfg.colors.sgr,
-            &cfg.colors.background.err,
-            &cfg.colors.text_light,
-            &cfg.icons.err,
-            &opt.error,
-            &cfg.colors.sgr
-        ),
-        err_job: format!("{}{}{}{}{}",
-            &cfg.colors.sgr,
-            &cfg.colors.foreground.err,
-            &cfg.colors.background.job,
-            &cfg.end_icon,
-            &cfg.colors.sgr
-        ),
-        err_dir: format!("{}{}{}{}{}",
-            &cfg.colors.sgr,
-            &cfg.colors.foreground.err,
-            &dir_bg,
-            &cfg.end_icon,
-            &cfg.colors.sgr
-        ),
-
-        job: format!("{}{}{} {} {} {}",
-            &cfg.colors.sgr,
-            &cfg.colors.background.job,
-            &cfg.colors.text_dark,
-            &cfg.icons.job,
-            &opt.jobs,
-            &cfg.colors.sgr
-        ),
-        job_dir: format!("{}{}{}{}{}",
-            &cfg.colors.sgr,
-            &cfg.colors.foreground.job,
-            &dir_bg,
-            &cfg.end_icon,
-            &cfg.colors.sgr
-        ),
-
-        dir: format!("{}{}{} {} {} {}",
-            &cfg.colors.sgr,
-            &dir_bg,
-            &dir_text,
-            &dir_icon,
-            &opt.cwd,
-            &cfg.colors.sgr
-        ),
-        dir_end: format!("{}{}{}{}",
-            &cfg.colors.sgr,
-            &dir_fg,
-            &cfg.end_icon,
-            &cfg.colors.sgr
-        ),
-        dir_sud: format!("{}{}{}{} {}{}{}{} ",
-            &cfg.colors.sgr,
-            &dir_fg,
-            &cfg.colors.background.sud,
-            cfg.end_icon,
-            &cfg.colors.sgr,
-            &cfg.colors.foreground.sud,
-            &cfg.icons.end_sud,
-            &cfg.colors.sgr
-        )
-    };
-    Ok(def_conf)
 }
--- .config/shell/vlkprompt-rs/src/main.rs
+++ .config/shell/vlkprompt-rs/src/main.rs
@@ -7,15 +7,17 @@ use std::{
 use serde_json;
 
 pub mod config;
-mod options;
+// mod options;
 mod init;
 
 #[derive(Debug, PartialEq, Clone)]
 struct ArgOptions {
     shell: String,
+    init: bool
 }
 fn build_arg_options() -> Result<ArgOptions, io::Error> {
     let mut arg_shell = String::new();
+    let mut arg_init = false;
     for arg in env::args().skip(1) {
         if let Some(position) = arg.find("=") {
             let (key, val) = arg.split_at(position + 1);
@@ -24,38 +26,96 @@ fn build_arg_options() -> Result<ArgOptions, io::Error> {
                 _ => continue,
             }
         }
+        else {
+            match arg.as_str() {
+                "--init" => arg_init = true,
+                _ => continue
+            }
+        }
     }
     Ok(ArgOptions {
-        shell: arg_shell
+        shell: arg_shell,
+        init: arg_init
     })
 }
 
 fn main() -> Result<(), Box<dyn error::Error>> {
     let arg_options = build_arg_options()?;
-    let res_opts = options::get_opts();
-    let res_config = retrieve_config_leaky();
-    if res_config.is_err() || res_opts.is_err() {
-        init(arg_options)?;
+    let conf = config::generate_config(&arg_options.shell)?;
+
+    // let opts = options::get_opts()?;
+    let res_opts = config::get_opts();
+    if res_opts.is_err() || arg_options.init {
+        init(&conf.shell)?;
         process::exit(0)
     }
+
     let opts = res_opts.unwrap();
-    let conf = res_config.unwrap();
-    // println!("{}Hello World{}", config.colors.bg_dir_normal, config.colors.sgr);
-    // println!("{:#?}", parsed_args);
+    // println!("{:#?}", opts);
+    // println!("{:#?}", conf);
+    print_prompt(conf, opts)?;
     Ok(())
 }
 
-fn init(arg_options: ArgOptions) -> Result<(), Box<dyn error::Error>> {
-    let current_shell = arg_options.shell;
-    let config = config::generate_config(&env::var("VLKPROMPT_SHELL").unwrap_or("bash".to_string()))?;
-    let json_config = serde_json::to_string(&config)?;
-    println!("export VLKPROMPT_CONFIG=\"{}\"", json_config.replace("\"", "\\\""));
-    println!("{}", init::INIT_PRECMD_SCRIPT);
+fn init(current_shell: &config::Shell) -> Result<(), Box<dyn error::Error>> {
+    println!("\n#VLKPROMPT INIT SCRIPT\n");
+    println!("{}", init::INIT_PRECMD_SCRIPT_ZSH);
     Ok(())
 }
 
-fn retrieve_config_leaky() -> Result<config::Config, Box<dyn error::Error>> { // config::Config
-    let env_config = env::var("VLKPROMPT_CONFIG").unwrap().leak();
-    let myconfig: config::Config = serde_json::from_str(env_config)?;
-    Ok(myconfig)
+// macro_rules! section_fmt {
+//     ($b:tt,$t:expr,$o:expr,$cfg:expr,$opt:expr) => {
+//         {
+//             let cfg = config.clone();
+//             let opt = options.clone();
+//             println!("{}{}{} {} {} {}",
+//                 $cfg.colors.sgr,
+//                 $cfg.colors.background.$b,
+//                 $cfg.colors.$t,
+//                 $cfg.icons.$b,
+//                 $opt.$o,
+//                 $cfg.colors.sgr
+//             );
+//         }
+//     };
+// }
+
+fn print_prompt(config: config::Config, options: config::Options) -> Result<(), Box<dyn error::Error>> {
+    // dir color
+    let mut prompt: Vec<String> = Vec::new();
+    // {
+    //     let cfg = config.clone();
+    //     let opt = options.clone();
+    //     println!("{}{}{} {} {} {}",
+    //         cfg.colors.sgr,
+    //         cfg.colors.background.err,
+    //         cfg.colors.text_light,
+    //         cfg.icons.err,
+    //         opt.error,
+    //         cfg.colors.sgr,
+    //     );
+    // }
+    let defined_config = config::define_config(config.clone(), options.clone())?;
+    if options.has_err {
+        prompt.push(defined_config.err);
+        if &options.has_jobs == &true {
+            prompt.push(defined_config.err_job)
+        }
+        else {
+            prompt.push(defined_config.err_dir)
+        }
+    }
+    if options.has_jobs {
+        prompt.push(defined_config.job)
+    }
+    prompt.push(defined_config.dir);
+    if options.has_sudo {
+        prompt.push(defined_config.dir_sud)
+    }
+    else {
+        prompt.push(defined_config.dir_end)
+    }
+    println!("{} hello", prompt.join(""));
+
+    Ok(())
 }
