e6f5954c78591d8c59128a33b7cfe715655d79fe
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-09-17 23:20:16 -0500

    Commit from shell alias at 09/17/23 11:20:16 PM

--- .config/bar-scripts/weather.sh
+++ .config/bar-scripts/weather.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/dash
-curl -s 'v2n.wttr.in?format=%c%t' | sed 's/ //g ; s/+/ / ; s/F//;s/ /  /g ; s/Unknown.*/ ?/'
+info="$(curl -s 'v2n.wttr.in?format=%c%t' | sed 's/ //g ; s/+/ / ; s/F//;s/ /  /g ; s/Unknown.*/ ?/')"
+case "$info" in 'Sorry'*) exit 1 ;; *) echo "$info" ;; esac
