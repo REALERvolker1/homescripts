b385d5546971c7564e0cda2e6bb3b2f34bc99412
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-09-21 16:57:15 -0500

    Commit from shell alias at 09/21/23 04:57:15 PM

--- bin/vlk-xrandr.sh
+++ bin/vlk-xrandr.sh
@@ -1,27 +1,83 @@
-#!/usr/bin/bash
+#!/usr/bin/dash
 # xrandr script so I don't ever have to use xlayoutdisplay again
-set -euo pipefail
-IFS=$'\n\t'
+set -eu
+IFS='
+'
 
+# name of the monitor
 PRIMARY='eDP-1'
+HIRATE=144
+LORATE=60
+RES='1920x1080'
+
+# --left-of, --right-of, --above, --below
+RELATIVE_POS='--left-of'
+
 # my hardware has an interesting quirk where if my external monitor is
 # plugged into my dedicated GPU port, it does not reduce the refresh rate
-# of my primary monitor.
+# of my primary monitor. This should be impossible, as xorg sees both
+# monitors as a single screen, but I'm not complaining
+# Less fortunate people can just set it to '' I think
+NORATEREDUCE=':HDMI-1-0:DP-1-0:'
 
-NORATEREDUCE=('HDMI-1-0' 'DP-1-0')
+rate="$HIRATE"
+has_primary=false
+# args="xrandr --output '$PRIMARY' --primary --auto"
+args=''
+previous="$PRIMARY"
 
-xrandr_info="$(
-    randr="$(xrandr 2>/dev/null)"
-    currenthead=''
-    echo "$randr" | while read -r line; do
-        if echo "$line" | grep -q '^[a-zA-Z]'; then
-            echo "$line"
-        fi
-    done
-)"
+for i in $(xrandr | grep -oP '^.*(?= connected)'); do
+    i_msg="detected monitor $i"
+    case "$i" in
+    "$PRIMARY")
+        i_msg="$i_msg -- \$PRIMARY"
+        # make sure we actually have the primary monitor here
+        has_primary=true
+        ;;
+    *)
+        case :"$i": in
+        *:"$NORATEREDUCE":*)
+            i_msg="$i_msg -- dGPU-connected"
+            ;;
+        *)
+            i_msg="$i_msg -- iGPU-connected"
+            rate=60
+            ;;
+        esac
+        args="$args --output '$i' --auto $RELATIVE_POS '$previous'"
+        previous="$i"
+        ;;
+    esac
+    echo "$i_msg"
+done
 
-echo "$xrandr_info"
+if ! $has_primary; then
+    echo "Error, \$PRIMARY monitor '$PRIMARY' not detected!"
+    exit 1
+fi
 
-# for i in $(xrandr | grep ' connected' | cut -d ' ' -f 1); do
-#     echo "=$i="
-# done
+# forcibly switch to the lower refresh rate if not plugged in
+case "$(cat /sys/class/power_supply/ACAD/online)" in
+0) rate="$LORATE" ;;
+*) rate="$HIRATE" ;;
+esac
+
+args="xrandr --output '$PRIMARY' --primary --mode '$RES' --rate '$rate' $args"
+
+echo "sh -c \"$args\""
+case "${1:-}" in
+'--dry-run')
+    echo "dry run -- command not run"
+    ;;
+'')
+    sh -c "$args"
+    # if pgrep 'picom' >/dev/null; then
+    #     killall picom
+    #     picom &
+    # fi
+    ;;
+*)
+    echo "${0##*/} --dry-run to dry-run, without args makes it run"
+    exit 0
+    ;;
+esac
