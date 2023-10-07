e7391892568972a2c7b0db09ace632849aff93b9
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-10-07 16:02:19 -0500

    Commit from dotfiles.sh at 10/07/23 04:02:18 PM

--- bin/test-grep.sh
+++ bin/test-grep.sh
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/usr/bin/dash
 
-sh -c "while true; do nvidia-smi >/dev/null; sleep 5; done" &
-thatpid="$!"
-
-sleep 10
-kill "$thatpid"
-
-wait
+active_monitor="$(hyprctl activeworkspace -j | jq -r '.monitor')"
+active_window="$(hyprctl activewindow -j | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')"
+sleep 1
+# grim -g "$(slurp)"
+# grim -o "$active_monitor" - | swappy -f -
+# grim
+grim -g
