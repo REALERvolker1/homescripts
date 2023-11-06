507838c717eb7bebf9c9538605dc8bfb0da72168
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-11-06 13:12:27 -0600

    Commit from dotfiles.sh at 11/06/23 01:12:18 PM

---
+++ bin/rustcfg.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# a script by vlk to automate building required rust dependencies
+
+if command -v cargo &>/dev/null; then
+    echo "CRITICAL ERROR! Required dependency 'cargo' not found!" | tee /dev/stderr
+    exit 1
+fi
+
+buildcfg() {
+    cd "$1" || return 1
+    cargo build --release
+    cd "$RUSTCFG_DIR" || return 1
+}
+
+RUSTCFG_DIR="${XDG_CONFIG_HOME:=$HOME/.config}/rustcfg"
+
+declare -a errors=()
+for i in "$RUSTCFG_DIR"/*; do
+    buildcfg "$i" || errors+=("$i")
+done
+
+if ((${#errors[@]})); then
+    printf '%s\n' \
+        "There were one or more errors building required rust packages" \
+        "${errors[@]}"
+    exit 1
+fi
