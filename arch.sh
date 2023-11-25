7ea92d407893af513e12008e69adcc036eaa36ad
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2023-11-25 00:46:49 -0600

    Commit from dotfiles.sh at 11/25/23 12:46:32 AM

--- arch.sh
+++ arch.sh
@@ -1,44 +1,26 @@
 #!/bin/bash
-# must be run as root
 
-if [[ ${USER:=$(whoami)} != 'root' ]]; then
-    echo "Error, this script must be run as root!"
-    exit 1
-fi
+#if [[ ${USER:=$(whoami)} != 'root' ]]; then
+#    echo "Error, this script must be run as root!"
+#    exit 1
+#fi
 
-exit 0
-for i in "$@"; do
-    case "${i:-}" in
-    '--livedisk')
-        lsblk
-        echo "Which drive do you want to install the bootloader on?"
-        read -r -p ' > ' boot
-        echo "Which drive do you want to install the main system on?"
-        read -r -p ' > ' system
-        printf '%s\n' \
-            "This installation script assumes you have made your partition table already and you want to use btrfs." \
-            "If you have already done so, if your name is vlk, and if you truly want to run this script, enter 'y'." \
-            'Otherwise the script will exit.'
-        read -r -p 'You sure you want to run this? >' LAST_WARNING
-        [[ ${LAST_WARNING:-} == y && -n ${system:+x} && -n ${boot:+x} ]] || exit 0
-        mkfs.btrfs -L 'archbtw' "$system"
-        mount "$system" /mnt
-        declare -A subvolumes=(
-            [/]=@
-            [/home]=@home
-            [/var]=@var
-        )
-        for i in "${subvolumes[@]}"; do
-            btrfs subvolume create "$i"
-        done
-        umount /mnt
-        for i in "${!subvolumes[@]}"; do
-            mkdir -p "/mnt${i}"
-            mount -o "relatime,compress=zstd:5,ssd,subvol=${subvolumes[$i]}" "/mnt${i}"
-        done
-    ;;
+# This script is really just a glorified documentation right now.
+cat <<ARCHBTW
 
-    '--bare-system') ;;
+EFI stuff
+mkfs.fat -F 32 /dev/efi_system_partition
 
-    esac
-done
+mount --mkdir /dev/efi_part /mnt/boot
+
+mkswap /dev/swap_partition
+swapon /dev/swap_partition
+
+pacstrap -K /mnt base linux linux-firmware intel-ucode grub sudo neovim btrfs-progs
+
+Mount options
+
+noatime,compress=zstd:5,ssd,subvol=@{,home,var}
+
+
+ARCHBTW
