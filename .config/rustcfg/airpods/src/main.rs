3419b701999f1b4a690c667e0fac389df9b2160d
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2024-04-24 15:12:02 -0500

    Commit from dotfiles.sh at 04/24/24 03:11:59 PM

--- .config/rustcfg/airpods/src/main.rs
+++ .config/rustcfg/airpods/src/main.rs
@@ -1,7 +1,20 @@
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
-    let conn = dbus::nonblock::Connection::
-    let pods = airinfo::find_pods().await.expect("Could not find airpods!");
+fn main() -> Result<(), dbus::Error> {
+    let (resource, conn) = dbus_tokio::connection::new_system_local()?;
 
-    println!("{:#?}", pods);
+    let async_main = async {
+        let pods = airinfo::find_pods().await.expect("Could not find airpods!");
+        println!("{:#?}", pods);
+
+        Ok::<(), dbus::Error>(())
+    };
+
+    let runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_time()
+        .enable_io()
+        .build()
+        .expect("Could not build the tokio runtime!");
+
+    runtime.block_on(async_main)?;
+
+    Ok(())
 }
